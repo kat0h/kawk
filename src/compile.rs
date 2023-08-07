ee5a49ac74146b4384b318aa159f5f606a5c44e9
Kota Kato <45391880+kat0h@users.noreply.github.com>
2023-08-07 14:29:47 +0900

    Add base function for compiler

--- src/compile.rs
+++ src/compile.rs
@@ -1,1 +1,34 @@
-pub fn compile() {}
+use crate::ast;
+use crate::vm::Opcode;
+
+pub fn compile(ast: &ast::Program) {
+    let vmprogram: Vec<Opcode> = vec![];
+
+    // BEGINパターンを探しコンパイル
+    compile_all_begin_pattern(ast, &vmprogram);
+}
+
+fn compile_all_begin_pattern(ast: &ast::Program, vmprogram: &Vec<Opcode>) {
+    let items = ast
+        .iter()
+        .filter(|i| matches!(i.pattern, ast::Pattern::Begin))
+        .collect::<Vec<_>>();
+
+    for item in items.into_iter() {
+        compile_action(&item.action, vmprogram);
+    }
+}
+
+fn compile_action(action: &ast::Action, vmprogram: &Vec<Opcode>) {
+    for statement in action.into_iter() {
+        match statement {
+            ast::Statement::Print(expressions) => {
+
+            }
+        }
+    }
+}
+
+fn compile_expression(expression: &ast::Expression, vmprogram: &Vec<Opcode>) {
+
+}
